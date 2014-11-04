4170b6a5dca116a0d925adaca5713cfca5d12180
luiarthur <luiarthur@yahoo.com>
2014-11-04 11:17:27 -0700

    yes

--- Stat666/HW/HW4/4.R
+++ Stat666/HW/HW4/4.R
@@ -1,3 +1,5 @@
+options("width"=150)
+
 # PSA
 X <- as.matrix(read.table("PSAData.txt",header=T))
 X <- cbind(1,X)
@@ -6,18 +8,82 @@ Y <- as.matrix(read.table("PSAContributions.txt",header=T))
 
 #1
 n <- nrow(X)
-k <- ncol(X)
+r <- ncol(X)
+q <- r-1
+p <- ncol(Y)
 
 B <- solve(t(X) %*% X) %*% t(X) %*% Y
 XB <- X %*% B
 Y.XB <- Y-XB
+E <- t(Y.XB) %*% Y.XB
 
-S <- t(Y.XB) %*% Y.XB / (n-k)
+S <-  E / (n-r)
 
 # Q1: Is there a significant relationship between the chemical abundances (X) and 
 #     the source of contributions (Y)? What is $\hat(B)$?
 
+# What is $\hat(B)$?
 library(xtable)
 xtab.B <- xtable(B)
 
+y.bar <- apply(Y,2,mean)
+H <- t(Y) %*% Y - n * y.bar %*% t(y.bar)
+
+lam <- det(E) / det(E+H)
+
+# lam can also be calculated this way:
+s <- min(p,q)
+l <- eigen(solve(E,H))$values[1:s]
+lam <- prod(1/(1+l))
+V <- sum(l/(1+l)) # Pillai
+
+
+lam.to.F <- function(lam,vh,ve,p) { # Approximation
+  t <- sqrt( (p^2*vh^2-4) / (p^2+vh^2-5) )
+  L <- lam^(1/t)
+  w <- ve + vh - (p+vh+1)/2
+
+  df <- c(p*vh, w*t-(p*vh-2)/2)
+
+  F.stat <- (1-L) / L * df[2]/df[1]
+  out <- c(F.stat,df)
+  names(out) <- c("F.stat","df1","df2")
+
+  out
+}
+
+V.2.F <- function(V,s,N,m) { # Approximation
+  F.stat <- (2*N + s + 1) / (2*m + s + 1) * V/(s-V)
+  df <- s * (c(m,N) + s + 1)
+  
+  out <- c(F.stat,df)
+  names(out) <- c("F.stat","df1","df2")
+  
+  out
+}
+
+F.L <- lam.to.F(lam,vh=q-1,ve=n-r,p=p)
+p.L <- 1-pf(F.L[1],F.L[2],F.L[3])
+
+F.V <- V.2.F(V,s=min(p,q),N=(n-q-p-2)/2,m=(abs(p-1)-1)/2)
+p.V <- 1-pf(F.V[1],F.V[2],F.V[3])
+
+# p.val < .05 => Yes, there is a significant contribution. H_0 B1 = O is rejected.
+
+#2 
+var.B.vec <- S %x% solve(t(X)%*%X)
+B0 <- B[1,]
+B1 <- B[-1,]
+
+# Another.Lambda <- det(cov(cbind(Y,X[,-1]))) / (det(var(X[,-1])) * det(var(Y))) # But why is it different?
+# We reject H0, and the first eigen value does not completely dominate the other eigen values. (See: l/sum(l))
+# l1+l2 accounts for 94% of the eigen values, which substantially dominates the other eigen values.
+# So, the essential dimensionality of the relationship between X and Y is 2.
+# This was not directly evident from inspecting B1, because B1 is large and it is difficult to see trends.
+
+sum(l[1:2]) / sum(l)
+
+
+#3: Canonical Correlation 
+#   OBJECTIVE: Summarize the linear relationship betweem the two groups of variables, Y & X.
 
