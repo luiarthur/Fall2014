bef19ff76cca1752bebad65a3e3d6d3134fa1be2
luiarthur <luiarthur@yahoo.com>
2014-11-12 16:51:43 -0700

    yes

--- Stat651/miniProject/4/mp4.R
+++ Stat651/miniProject/4/mp4.R
@@ -26,7 +26,7 @@ rig <- function(n,a,b) 1/rgamma(n,a,scale=b)
   m <- 4.5 # => E[mu] = 4.5
   s2 <- 2  # => V[mu] = 2
   as <- 5/2 # => E[sig2] = 1
-  bs <- 3/2 # => V[sig2] = 2
+  bs <- 2/3 # => V[sig2] = 2
   at <- 13/2 # => E[tau2] = 3
   bt <- 2/33 # => V[tau2] = 2
 
@@ -40,7 +40,7 @@ rig <- function(n,a,b) 1/rgamma(n,a,scale=b)
     plot(density(prior.y),lwd=3,col="red",main="Prior Predictive")
     color.den(density(prior.y),5,max(density(prior.y)$x),"red")
     text(6,.1,round(mean(prior.y>5),4),cex=2)
-    #abline(v=mean(prior.y)) # mean about 4.5
+    #abline(v=mean(prior.y)) # mean about 4.5, var about 3.56
   dev.off()
 
 gibbs <- function(B=1e5) {
@@ -132,19 +132,21 @@ M.cov <- var(M)
 pdf("out/postMean.pdf")  
   plot(M.mean[1:23],col="blue",pch=20,
        ylim=c(min(M.lower[1:23]),max(M.upper[1:23])),
-       type="o",main="Posterior Mean Theta's")
+       type="o",main=expression(paste("Posterior Mean ",theta,"'s")),xlab="i",
+       ylab=expression(theta[i]))
   lines(M.upper[1:23],col="red",type="b",pch=20)
   lines(M.lower[1:23],col="red",type="b",pch=20)
 dev.off()
+
 sink("out/postMeanMu.tex")
   P <- cbind(M.mean[24:26],M.lower[24:26],M.upper[24:26],diag(M.cov)[24:26])
-  colnames(P) <- c("Mean","CI.lower","CI.upper","Var")
-  rownames(P) <- c("\\mu","\\sigma^2","\\tau^2")
-  xtable(P,digits=6)
+  colnames(P) <- c("Mean","CI.lower","CI.upper","Variance")
+  rownames(P) <- c("$\\mu$","$\\sigma^2$","$\\tau^2$")
+  print(xtable(P,digits=6,caption="Posterior Mean \\& Variance"),sanitize.t=function(x)x,caption.p="top")
 sink()
 
+library(MASS)
 plot.hyper <- function(m,names=NULL) {
-  library(MASS)
   k <- ncol(m)
   par(mfrow=c(k,k))
   for (i in 1:k) {
@@ -154,13 +156,19 @@ plot.hyper <- function(m,names=NULL) {
         K <- kde2d(m[,i],m[,j])
         contour(K,col="red",main=name,cex.main=.9,xlab=names[i],ylab=names[j])
       } else if (i>j) {
-        #plot(m[,i],m[,j],type="l",col="pink",main=name,cex.main=.9,xlab=names[i],ylab=names[j])
-        plot.new()
-        par(usr=c(0,1,0,1))
-        text(.5,.5,round(cov(m[,i],m[,j]),4))
-        box()
-        name <- ifelse(is.null(names),"",paste("Covariance between \n",names[i],"&",names[j]))
-        title(name,cex.main=.9)
+        name <- ifelse(is.null(names),"",
+                paste("Trace Plot Between \n",names[i],"&",names[j]))
+        plot(m[,i],m[,j],type="l",col="pink",main=name,cex.main=.9,
+             xlab=names[i],ylab=names[j])
+        points(m[1,i],m[1,j],pch="B")     
+        points(m[nrow(m),i],m[nrow(m),j],pch="E")     
+        #plot.new()
+        #par(usr=c(0,1,0,1))
+        #text(.5,.5,round(cov(m[,i],m[,j]),4))
+        #box()
+        #name <- ifelse(is.null(names),"",
+        #        paste("Covariance between \n",names[i],"&",names[j]))
+        #title(name,cex.main=.9)
 
       } else {
         name <- ifelse(is.null(names),"",paste("Posterior Density for",names[i]))
@@ -173,10 +181,41 @@ plot.hyper <- function(m,names=NULL) {
 
 
 
-pdf("out/postVarTrace.pdf")
+pdf("out/postDist.pdf")
   plot.hyper(m<-M[(nrow(M)*.8):nrow(M),24:26],c("mu","sigma2","tau2"))
   #plot.hyper(m<-M[(nrow(M)*.8):nrow(M),22:26],c("theta22","theta23","mu","sigma2","tau2"))
   #plot.hyper(m<-M[(nrow(M)*.8):nrow(M),23:26],c("theta23","mu","sigma2","tau2"))
+dev.off()
+
+sink("out/meanVar.tex")
+  mv <- cbind(M.mean,diag(M.cov))
+  rownames(mv) <- c(paste0("$\\theta_{",1:23,"}$"),
+                    "$\\mu$","$\\sigma^2$","$\\tau^2$")
+  colnames(mv) <- c("Mean","Variance")
+  print(xtable(mv,digits=6,caption="Posterior Mean \\& Variance"),
+        sanitize.t=function(x)x,caption.p="top")
+sink()
+
+pdf("out/postDistUni.pdf")
+  plot(density(M[,1]),col="blue",lwd=3,
+       expression(paste("Posterior Distribution For ",theta[1])))
+dev.off()  
+pdf("out/traceUni.pdf")  
+  plot(M[,1],type="l",col="purple",
+       main=expression(paste("Trace Plot For ",theta[1])))
+dev.off()
+
+pdf("out/trace.pdf")
+  par(mfrow=c(4,1))
+    plot(M[,23],type="l",col="purple",ylab=expression(theta[23]),
+         main=expression(paste("Trace Plot For ",theta[23])))
+    plot(M[,24],type="l",col="purple",ylab=expression(mu),
+         main=expression(paste("Trace Plot For ",mu)))
+    plot(M[,25],type="l",col="purple",ylab=expression(sigma^2),
+         main=expression(paste("Trace Plot For ",sigma^2)))
+    plot(M[,26],type="l",col="purple",ylab=expression(tau^2),
+         main=expression(paste("Trace Plot For ",tau^2)))
+  par(mfrow=c(1,1))
 dev.off()
 
 #4: Posterior Predictive
@@ -195,6 +234,25 @@ pdf("out/postPred.pdf")
   plot(post.pred.den,lwd=3,col="blue",main="Posterior Predictive for Next Average Faculty Evaluation")
   color.den(post.pred.den,5,mx,col="blue")
   text(5.8,.2,round(p.gt.5,4),cex=2)
+dev.off()
+
+a.image <- function(Q,color=paste0("gray",0:100),...) {
+  library(fields)
+  pr <- par("mar")
+  par(mar=c(5,4.5,4,7))
+  image(t(apply(Q,2,rev)),yaxt="n",xaxt="n",col=color,...)
+  axis(2,at=seq(0,1,length.out=ncol(Q)),labels=rev(colnames(Q)),las=2)
+  axis(3,at=seq(0,1,length.out=nrow(Q)),labels=rownames(Q),las=2)
+  image.plot(Q,legend.only=T,col=color)
+  par(mar=pr)
+}
+
+cov.M <- cov(M)
+rownames(cov.M) <- c(paste0("theta",1:23),"mu","sig2","tau2")
+colnames(cov.M) <- rownames(cov.M)
+
+pdf("cov.pdf")
+  a.image(cov.M,col=tim.colors(12))
 dev.off()
 
 
