e5743f3d0d7fc1d6d7e05dbc651d85a514bdfcd0
luiarthur <luiarthur@yahoo.com>
2015-01-20 17:52:33 -0700

    yes

--- Stat637/aids.R
+++ Stat637/aids.R
@@ -14,10 +14,46 @@ plot(log(x),log(y),pch=20,cex=1.5,main="Log Number of Cases Vs.
 ############################################################################
 
 # 1c)
+iwls <- function(X,y,b=NULL,eps=1e-5,maxits=500) {
+  X <- cbind(1,X)
+  if (is.null(b)) b <- matrix(0,ncol(X),1)
+  diff <- 1
+  its <- 0
 
+  while(diff>eps && its<maxits) {
+    old.b <- b
+    Xb <- X%*%b
+    W <- diag(as.numeric(exp(Xb)))
+    z <- Xb + y/exp(Xb) - 1
+    tW <- t(X)%*%W
+    b <- solve(tW%*%X,tW %*% z)
+    diff <- sqrt(sum((b-old.b)^2))
+    its <- its + 1
+  }
 
-iwls <- function(X,b=rep(0,ncol(X)),z) {
-  X <- cbind(1,log(X))
-  W <- exp(X%*%b)
-   
+  rownames(b) <- c("Intercept","Slope")
+  cat("Iterations: ",its,"\n")
+  cov.b <- solve(tW%*%X)
+  colnames(cov.b) <- rownames(cov.b) <- rownames(b)
+
+  list("b"=b,"cov"=cov.b)
 }
+
+out <- iwls(log(x),y)
+out$b
+
+# 1d)
+poismod <- glm(y~log(x),family=poisson)
+summary(poismod)
+
+# 1e)
+out$cov
+M <- matrix(0,2,3)
+M[1,] <- c(out$b[1],qnorm(c(.025,.975),out$b[1],sqrt(diag(out$cov)[1])))
+M[2,] <- c(out$b[2],qnorm(c(.025,.975),out$b[2],sqrt(diag(out$cov)[2])))
+colnames(M) <- c("Estimate","Lower.CI","Upper.CI")
+rownames(M) <- c("Intercept","Slope")
+M
+
+# Since neither confidence intervals contain 0, both betas are significant at
+# the 95% confidence level.
