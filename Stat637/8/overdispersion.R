3bb5db0a8f87536da76608ba78079b1a98aaad58
luiarthur <luiarthur@gmail.com>
2015-03-29 10:56:31 -0600

    yes

--- Stat637/8/overdispersion.R
+++ Stat637/8/overdispersion.R
@@ -1,4 +1,24 @@
 library(aod)
+library(MASS) # family=negative.binomial
 head(dja)
 
-glm(y/n ~ group, family=quasibinomial, data=dja)
+#1 
+mod1 <- glm(y/n ~ group, family=quasibinomial(link="logit"), weights=n, data=dja)
+summary(mod1)
+
+#2
+mod2 <- betabin(cbind(y,n-y) ~ group, ~1, link="logit", data=dja)
+summary(mod2)
+
+#3
+mod3 <- glm(y ~ group, family=quasipoisson(link="log"), offset=trisk, data=dja)
+summary(mod3)
+
+#4
+mod4 <- glm(y ~ group, family=negative.binomial(theta=1), offset=trisk, data=dja)
+summary(mod4)
+
+#5
+# Which model do I prefer:
+#  - mdoel diagnostics
+#  - similarities / differences in moels
