1fb0be0e719b79457851ee8d5bd32bf3b251f798
luiarthur <luiarthur@yahoo.com>
2015-04-01 21:06:29 -0600

    yes

--- Stat637/mp/logisticMultMod.R
+++ Stat637/mp/logisticMultMod.R
@@ -16,31 +16,41 @@ dat <- merge(rad,ctyInfo,by.x="county",by.y="cty")
 
 N <- nrow(dat)
 J <- length(unique(dat$county))
+thresh <- 1.8
 y <- log(dat$activity)
+z <- ifelse(y>1.8,1,0)
 x <- ifelse(dat$basement=="Y",1,0)
 counties <- unique(dat$county)
-
-G <- matrix(0,N,J)
 u <- log(unique(dat$Uppm))
+
 Y <- as.list(1:J)
+Z <- as.list(1:J)
 X <- as.list(1:J)
 for (j in 1:J) {
   Y[[j]] <- y[which(dat$county==counties[j])]
+  Z[[j]] <- z[which(dat$county==counties[j])]
   X[[j]] <- x[which(dat$county==counties[j])]
 }
 
 
-mh <- function(B=1e3,csa=rep(.1,J),csb=.1,csg0=.1,csg1=.1,cssy=.1,cssa=.01,
-               sigb2=100,ubg0=100,ubg1=100,ubsy2=100,ubsa2=100) {
+# Logistic Veresion
+mh <- function(B=1e3,csa=rep(.1,J),csb=.1,csg0=.1,csg1=.1,cssa=.01,
+               sigb2=100,) {
   # Likelihoods:
-  # yij ~ N(aj+b*xij, sy2)  ......(1)
-    ll1 <- function(j,aj,b,sy2) sum(dnorm(Y[[j]],aj+b*X[[j]],sqrt(sy2),log=T))
+  # zij ~ Bern(pij)  ......(1)
+    ll1 <- function(j,zj,aj,b) {
+      pj <- exp(aj+b*X[[j]]) / (1+exp(aj+b*X[[j]]))
+      sum(dbinom(zj,1,pj,log=T))
+    }  
   #  aj ~ N(g0+g1*uj, sa2) ......(2)
     ll2 <- function(aj,g0,g1,uj,sa2) dnorm(aj,g0+g1*uj,sqrt(sa2),log=T)
   # y ~ prod_{j=1:J}{i=1:N}{N(aj+b*xij, sy2)}  ......(3)
-    ll3 <- function(a,b,sy2) {
+    ll3 <- function(a,b) {
       out <- 0
-      for (j in 1:J) out <- out + sum(dnorm(Y[[j]],a[j]+b*X[[j]],sqrt(sy2),log=T))
+      for (j in 1:J) {
+        pj <- exp(a[j]+b*X[[j]]) / (1+exp(a[j]+b*X[[j]]))
+        out <- out + sum(dbinom(Z[[j]],1,pj,log=T))
+      }  
       out
     }
   #  a ~ prod_{j=1:J}{N(g0+g1*uj, sa2)} ......(4)
@@ -64,108 +74,91 @@ mh <- function(B=1e3,csa=rep(.1,J),csb=.1,csg0=.1,csg1=.1,cssy=.1,cssa=.01,
   b <- rep(0,B)
   g0 <- rep(1,B)
   g1 <- rep(1,B)
-  sy2 <- rep(1,B)
   sa2 <- rep(1,B)
 
   acc.a <- rep(0,J)
   acc.b <- 0
   acc.g0 <- 0
   acc.g1 <- 0
-  acc.sy2 <- 0
   acc.sa2 <- 0
   
-  for (z in 2:B) {
+  for (l in 2:B) {
     ot <- Sys.time()
 
-    a[z,] <- a[z-1,]
-    b[z] <- b[z-1]
-    g0[z] <- g0[z-1]
-    g1[z] <- g1[z-1]
-    sy2[z] <- sy2[z-1]
-    sa2[z] <- sa2[z-1]
+    a[l,] <- a[l-1,]
+    b[l] <- b[l-1]
+    g0[l] <- g0[l-1]
+    g1[l] <- g1[l-1]
+    sa2[l] <- sa2[l-1]
 
     # Update a:
     for (j in 1:J) {
-      cand <- rnorm(1,a[z,j],sqrt(csa[j]))
-      lr <- ll1(j,cand,  b[z],sy2[z]) + laj(cand,  g0[z],g1[z],u[j],sa2[z]) -
-           (ll1(j,a[z,j],b[z],sy2[z]) + laj(a[z,j],g0[z],g1[z],u[j],sa2[z]))
+      cand <- rnorm(1,a[l,j],sqrt(csa[j]))
+      lr <- ll1(j,Z[[j]],cand,  b[l]) + laj(cand,  g0[l],g1[l],u[j],sa2[l]) -
+           (ll1(j,Z[[j]],a[l,j],b[l]) + laj(a[l,j],g0[l],g1[l],u[j],sa2[l]))
       if (lr > log(runif(1))) {
-        a[z,j] <- cand
+        a[l,j] <- cand
         acc.a[j] <- acc.a[j]+1
       }
     } # End of J Loops
 
     # Update b:
-    cand <- rnorm(1,b[z],csb)
-    lr <- ll3(a[z,],cand,sy2[z]) + lb(cand) - 
-       (ll3(a[z,],b[z],sy2[z]) + lb(b[z]))
+    cand <- rnorm(1,b[l],csb)
+    lr <- ll3(a[l,],cand) + lb(cand) - 
+       (ll3(a[l,],b[l]) + lb(b[l]))
     if (lr > log(runif(1))) {
-      b[z] <- cand
+      b[l] <- cand
       acc.b <- acc.b+1
     }
 
-    # Update sy2:
-    cand <- rnorm(1,sy2[z],cssy)
-    if (cand>0){
-      lr <- ll3(a[z,],b[z],cand) + lsy2(cand) - 
-           (ll3(a[z,],b[z],sy2[z]) + lsy2(sy2[z]))
-      if (lr > log(runif(1))) {
-        sy2[z] <- cand
-        acc.sy2 <- acc.sy2+1
-      }
-    }
-
     # Update sa2:
-    cand <- rnorm(1,sa2[z],cssa)
+    cand <- rnorm(1,sa2[l],cssa)
     if (cand>0){
-      lr <- ll4(a[z,],g0[z],g1[z],cand) + lsa2(cand) - 
-           (ll4(a[z,],g0[z],g1[z],sa2[z]) + lsa2(sa2[z]))
+      lr <- ll4(a[l,],g0[l],g1[l],cand) + lsa2(cand) - 
+           (ll4(a[l,],g0[l],g1[l],sa2[l]) + lsa2(sa2[l]))
       if (lr > log(runif(1))) {
-        sa2[z] <- cand
+        sa2[l] <- cand
         acc.sa2 <- acc.sa2+1
       }
     }
 
     # Update g0:
-    cand <- rnorm(1,g0[z],csg0)
-    lr <- ll4(a[z,],cand,g1[z],sa2[z]) + lg0(cand) - 
-         (ll4(a[z,],g0[z],g1[z],sa2[z]) + lg0(g0[z]))
+    cand <- rnorm(1,g0[l],csg0)
+    lr <- ll4(a[l,],cand,g1[l],sa2[l]) + lg0(cand) - 
+         (ll4(a[l,],g0[l],g1[l],sa2[l]) + lg0(g0[l]))
     if (lr > log(runif(1))) {
-      g0[z] <- cand
+      g0[l] <- cand
       acc.g0 <- acc.g0+1
     }
 
     # Update g1:
-    cand <- rnorm(1,g1[z],csg1)
-    lr <- ll4(a[z,],g0[z],cand,sa2[z]) + lg1(cand) - 
-         (ll4(a[z,],g0[z],g1[z],sa2[z]) + lg1(g1[z]))
+    cand <- rnorm(1,g1[l],csg1)
+    lr <- ll4(a[l,],g0[l],cand,sa2[l]) + lg1(cand) - 
+         (ll4(a[l,],g0[l],g1[l],sa2[l]) + lg1(g1[l]))
     if (lr > log(runif(1))) {
-      g1[z] <- cand
+      g1[l] <- cand
       acc.g1 <- acc.g1+1
     }
-    count.down(ot,z,B)
+    count.down(ot,l,B)
   }# End of B Loops
   
-  out <- list("a"=a,"b"=b,"g0"=g0,"g1"=g1,"sy2"=sy2,"sa2"=sa2,
+  out <- list("a"=a,"b"=b,"g0"=g0,"g1"=g1,"sa2"=sa2,
               "acc.a"=acc.a/B,"acc.b"=acc.b/B,"acc.g0"=acc.g0/B,"acc.g1"=acc.g1/B,
-              "acc.sy2"=acc.sy2/B,"acc.sa2"=acc.sa2/B)
+              "acc.sa2"=acc.sa2/B)
   out
 }
 
-out <- mh(B=1e5)
-pdf("latex/images/apost.pdf")
+out <- mh(B=1e4)
+pdf("latex/images/lapost.pdf")
   plot.posts(out$a[,c(1,50,85)],names=c("a1","a50","a85"),color="pink",cex.legend=.5)
 dev.off()
-pdf("latex/images/bpost.pdf")
+pdf("latex/images/lbpost.pdf")
   plot.posts(out$b,names="b")
 dev.off()
-pdf("latex/images/gpost.pdf")
+pdf("latex/images/lgpost.pdf")
   plot.posts(cbind(out$g0,out$g1),names=c("g0","g1"))
 dev.off() 
-pdf("latex/images/sy2post.pdf")
-  plot.posts(out$sy2,names="sy2")
-dev.off()
-pdf("latex/images/sa2post.pdf")
+pdf("latex/images/lsa2post.pdf")
   plot.posts(out$sa2,names="sa2") # With uniform prior, trace plot is terrible; and acceptance rate is high (.8)
 dev.off()
 
@@ -173,19 +166,29 @@ out$acc.a
 out$acc.b
 out$acc.g0
 out$acc.g1
-out$acc.sy2
 out$acc.sa2
 
 a.h <- apply(out$a,2,mean)
+b.h <- mean(out$b)
 g0.h <- mean(out$g0)
 g1.h <- mean(out$g1)
+p.h1 <- apply(as.matrix(a.h),1,function(a) exp(a+b.h)/(1+exp(a+b.h)))
+p.h0 <- apply(as.matrix(a.h),1,function(a) exp(a)/(1+exp(a)))
 
-pdf("latex/images/au.pdf")
+cty <- sapply(counties,as.character)
+p.m <- cbind(as.factor(cty),p.h1,p.h0)
+p.m <- p.m[order(p.m[,2],p.m[,3]),]
+
+plot(p.m[,2],type="l",col="red",lwd=3,ylab="Probability of Danger",xaxt="n",xlab="")
+lines(p.m[,3],type="l",col="blue",lwd=3)
+axis(1,at=1:J,lab=cty[p.m[,1]],las=2,cex.axis=.5)
+legend("topleft",legend=c("Basement","No Basement"),col=c("red","blue"),bty="n",lwd=3)
+
+#pdf("latex/images/lau.pdf")
   plot(u,a.h,main="aj vs. log(Uranium)",pch=20,col="grey30",
        ylab="Regression Intercept (aj)",xlab="County-level Log Uranium Meaure")
   abline(g0.h,g1.h,lwd=2,col="purple")
   legend("topleft",bty="n",
          legend=c(paste("Intercept:",round(g0.h,4)),
                   paste("Slope:",round(g1.h,4))))
-dev.off()
-
+#dev.off()
