26e58116067ee1675e24e8bcaffa3e19380fc193
luiarthur <luiarthur@yahoo.com>
2015-04-03 18:00:19 -0600

    yes

--- Stat635/project/mm/R/gibbs2.R
+++ Stat635/project/mm/R/gibbs2.R
@@ -182,7 +182,7 @@ gibbs.post <- function(y,X,sigg2=100,sigb2=100,a=1,B=1000,burn=B*.1,showProgress
 # SIMULATIONS STUDY: UNCOMMENT TO SIMULATE!!!
 # Works when sig.r2=1
 source("genData.R")
-B <- 100
+B <- 200
 elapsed.time <- system.time(out <- gibbs.post(y,X,cs.r2=.2,B=B,showProgress=T,plotProgress=T))
 
 EZ <- est.Z(out$Zs)
